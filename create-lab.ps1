4b40806a43d6ec2ce36c8ad2b9d541cd3239d0a0
Thomsen <cthomsen@novellaclinical.com>
2021-04-14 07:59:49 -0400

    Updated

--- create-lab.ps1
+++ create-lab.ps1
@@ -4,10 +4,18 @@ CMT 13-April-2021
 #>
 
 #Create a resource group
-New-AzResourceGroup -Name "Lab-1" -Location                  
+New-AzResourceGroup -Name "Lab-1" -Location 'East US'
+
 #Create VNets
+New-AzVirtualNetwork -Name "Chad" -Location 'East US' -ResourceGroupName "Lab-1" -AddressPrefix "10.0.0.0/16"
+New-AzVirtualNetwork -Name "Michael" -Location 'East US' -ResourceGroupName "Lab-1" -AddressPrefix "10.1.0.0/16"
+
+#Create Subnets
+New-AzVirtualNetworkSubnetConfig -AddressPrefix "10.0.0.0/24" -Name "Chad-0" -
 
 #Create VMs
+New-AzVM -Name "Chad-VM" -
+
 
 #Add locks
 

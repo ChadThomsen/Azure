24e75dee7b07e56605f52c0f481a1a27ead94128
Chad Thomsen <chad.thomsen@gmail.com>
2021-04-13 22:04:38 -0400

    update

--- create-lab.ps1
+++ create-lab.ps1
@@ -4,7 +4,7 @@ CMT 13-April-2021
 #>
 
 #Create a resource group
-New-AzResourceGroup -Name "Lab-1" -Location                   
+New-AzResourceGroup -Name "Lab-1" -Location                  
 #Create VNets
 
 #Create VMs

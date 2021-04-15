6c9647f1399693bf95d0de57ad10512687737412
Chad Thomsen <chad.thomsen@gmail.com>
2021-04-15 07:32:40 -0400

    updated

--- create-lab.ps1
+++ create-lab.ps1
@@ -2,19 +2,25 @@
 This creates a lab to play with
 CMT 13-April-2021
 #>
+$rgname = "Lab-1"
 
 #Create a resource group
-New-AzResourceGroup -Name "Lab-1" -Location 'East US'
+New-AzResourceGroup -Name $rgname -Location 'East US'
+
+#Create Public IPs
+$PubIP = New-AzPublicIpAddress -Name "Public-IP" -ResourceGroupName $rgname `
+    -Location "eastus2" -Sku "Standard" -IdleTimeoutInMinutes 4 -AllocationMethod "static"
 
 #Create VNets
-New-AzVirtualNetwork -Name "Chad" -Location 'East US' -ResourceGroupName "Lab-1" -AddressPrefix "10.0.0.0/16"
-New-AzVirtualNetwork -Name "Michael" -Location 'East US' -ResourceGroupName "Lab-1" -AddressPrefix "10.1.0.0/16"
+New-AzVirtualNetwork -Name "Chad" -Location 'East US' -ResourceGroupName $rgname -AddressPrefix "10.0.0.0/16"
+New-AzVirtualNetwork -Name "Michael" -Location 'East US' -ResourceGroupName $rgname -AddressPrefix "10.1.0.0/16"
 
 #Create Subnets
-New-AzVirtualNetworkSubnetConfig -AddressPrefix "10.0.0.0/24" -Name "Chad-0" -
+$chadsub = New-AzVirtualNetworkSubnetConfig -AddressPrefix "10.0.0.0/24" -Name "Chad"
+$michaelsub = New-AzVirtualNetworkSubnetConfig -AddressPrefix "10.0.0.0/24" -Name "Michael" 
 
 #Create VMs   
-New-AzVM -Name "Chad-VM" -AddressPrefix  
+#New-AzVM -Name "Chad-VM" -AddressPrefix  
 
 
 #Add locks
